aed17c3a39c754f3b77e218ee9c44444165dc781
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-10-24 21:37:09 +0300

    24_10

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/*
+Task1
+ */
+//public class Main {
+//    public static void main(String[] args) {
+//    String name;
+//    int age;
+//    String city;
+//    }
+//}
+
+/*
+Task2
+ */
+
+//public class Main {
+//    public static void main(String[] args) {
+//    System.out.println("3118");
+//    }
+//}
+
+/*
+Task3
+ */
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int intValue = 2;
+//        int numericValue = 3;
+//        int size = 4;
+//        int number = 5;
+//    }
+//}
+
+/*
+Task4  Полнометражная картина
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int intValue = 2;
+        int numericValue = 3;
+        int size = 4;
+        int number = 5;
+    }
+}
