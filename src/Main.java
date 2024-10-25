0d3f9a2853833b159633bc24b628d674eafbb0f4
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-10-25 22:03:08 +0300

    25_10

--- src/Main.java
+++ src/Main.java
@@ -64,12 +64,61 @@ Task5    Последняя цифра числа
 Task6   9-ка это перевернутая 6-ка
  */
 
+//public class Main {
+//    public static void main(String[] args) {
+//        int six   = 6;
+//        six ++;
+//        six ++;
+//        six ++;
+//        System.out.println(six );
+//    }
+//}
+
+/*
+Task14    Причешем резюме (перевод заглавных букв в строчные)
+ */
+//public class Main {
+//    public static void main(String[] args) {
+//    String title = "MY NAME";
+//    String degree  = "IS NICK";
+//    String career = "I AM PROGRAMMER";
+//        System.out.println(title.toLowerCase());
+//        System.out.println(degree.toLowerCase());
+//        System.out.println(career.toLowerCase());
+//    }
+//}
+
+/*
+Task15   Попрошу регистр на меня не повышать!
+ */
+
+//public class Main {
+//    public static void main(String[] args) {
+//        String title = "my name";
+//        String degree  = "is nick";
+//        String career = "i am programmer";
+//        System.out.println(title.toUpperCase());
+//        System.out.println(degree.toUpperCase());
+//        System.out.println(career.toUpperCase());
+//    }
+//}
+
+
+/*
+Task16     Чтение и преобразование строк
+*/
 public class Main {
     public static void main(String[] args) {
-        int six   = 6;
-        six ++;
-        six ++;
-        six ++;
-        System.out.println(six );
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        String result = scanner2.nextLine();
+        String result1 = scanner.nextLine();
+        String result2 = scanner1.nextLine();
+
+        System.out.println(result);
+        System.out.println(result1.toUpperCase());
+        System.out.println(result2.toLowerCase());
+
     }
 }
